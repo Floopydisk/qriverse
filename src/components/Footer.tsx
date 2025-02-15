ed09f96a2193e847342eb2097cc34db90d7042a5
Ibukun-Odunsi Semiloore <109909221+Floopydisk@users.noreply.github.com>
2025-02-15 18:41:13 +0100

    flex

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,9 +41,9 @@ const Footer = () => {
           </a>
         </div>
       </div>
-      <div className="copyright">
-            &copy; {currentYear} ibukunodunsi 
-      </div>
+      {/* <div className="copyright">
+            &copy; {currentYear} 
+      </div> */}
     </footer>
   );
 };
