49e624189d7d3e4f150d4c25403b45855f39cf7f
Ibukun-Odunsi Semiloore <109909221+Floopydisk@users.noreply.github.com>
2025-05-19 17:40:34 +0100

    feat: finished setting up google authentication for QrLabs. (this commit is just an indicator. Actual feat was done on supabase and Google Cloud Console)

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -124,6 +124,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         options: {
           redirectTo: `${window.location.origin}/dashboard`,
         }
+        
       });
       
       if (error) throw error;
